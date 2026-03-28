8b9a2a55b7ada2385f7542c980ee3359d5e60787
crystal84187 <crystal.johnson18@icloud.com>
2026-03-27 23:29:40 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,8 @@
 const express = require('express');
 const app = express();
-
 app.use(require('cors')());
 app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.json({ status: 'DropFlow running!' });
+app.get('/', function(req, res) {
+  res.json({ status: 'OK' });
 });
-
-app.listen(process.env.PORT || 3000, () => {
-  console.log('Server started');
-});​​​​​​​​​​​​​​​​
+app.listen(process.env.PORT || 3000);
